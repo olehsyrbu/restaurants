355e0c269255b3dbe117263ae6cf5f7439881718
OlehSirbu <oleh@bookinglayer.com>
2024-05-05 22:08:39 +0200

    Refactor SearchProductsList component to use smaller image size and adjust button styling

--- src/components/SearchProducts/SearchProductList.tsx
+++ src/components/SearchProducts/SearchProductList.tsx
@@ -8,35 +8,33 @@ interface SearchProductsListProps {
   onSelect: (categoryName: string) => void;
 }
 
-const SearchProductsList = ({ items, onSelect }: SearchProductsListProps) => {
-  return (
-    <>
-      {items.length > 0 ? (
-        items.map((product) => (
-          <div
-            className="grid grid-cols-[4rem,1fr,2rem] gap-4"
+const SearchProductsList = ({ items, onSelect }: SearchProductsListProps) => (
+  <>
+    {items.length > 0 ? (
+      items.map((product) => (
+        <div
+          className="grid grid-cols-[4rem,1fr,2rem] gap-4"
+          key={product.name}
+        >
+          <ProductItem
             key={product.name}
+            product={product}
+            imageClassName="h-[3.75rem] w-[3.75rem]"
           >
-            <ProductItem
-              key={product.name}
-              product={product}
-              imageClassName="h-[60px] w-[60px]"
-            >
-              <button onClick={() => onSelect(product.name)}>
-                <img
-                  src={BackIcon}
-                  alt="back icon"
-                  className="rotate-180 rounded-full bg-[#262638] w-8 h-8 p-[8px]"
-                />
-              </button>
-            </ProductItem>
-          </div>
-        ))
-      ) : (
-        <Warning>No products found</Warning>
-      )}
-    </>
-  );
-};
+            <button onClick={() => onSelect(product.name)}>
+              <img
+                src={BackIcon}
+                alt="back icon"
+                className="rotate-180 rounded-full bg-[#262638] w-8 h-8 p-[0.5rem]"
+              />
+            </button>
+          </ProductItem>
+        </div>
+      ))
+    ) : (
+      <Warning>No products found</Warning>
+    )}
+  </>
+);
 
 export default SearchProductsList;

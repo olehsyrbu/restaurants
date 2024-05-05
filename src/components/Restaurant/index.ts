56c51c2ea5067f78679160d564f74a938a781b29
OlehSirbu <oleh@bookinglayer.com>
2024-05-05 22:06:40 +0200

    add RList to default

--- src/components/Restaurant/index.ts
+++ src/components/Restaurant/index.ts
@@ -2,10 +2,12 @@ import RestaurantCard from './RestaurantCard';
 import RestaurantInfo from './RestaurantInfo';
 import RestaurantActions from './RestaurantActions';
 import RestaurantCatalogs from './RestaurantCatalogs';
+import RestaurantList from './RestaurantList';
 
 export {
   RestaurantCard,
   RestaurantInfo,
   RestaurantActions,
   RestaurantCatalogs,
+  RestaurantList,
 };

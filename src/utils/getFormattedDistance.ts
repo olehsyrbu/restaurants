29f96be271fdea1ce3ed98a676b09a6a30788521
OlehSirbu <oleh@bookinglayer.com>
2024-05-05 22:07:05 +0200

    Refactor getFormattedDistance function to accept distance as a parameter

--- src/utils/getFormattedDistance.ts
+++ src/utils/getFormattedDistance.ts
@@ -1,12 +1,7 @@
-import { getDistance } from 'geolib';
-import { Point } from '@src/types';
 
 export default function getFormattedDistance(
-  userLocation: Point | null,
-  coordinates: Point
+  distance: number
 ) {
-  const distance = userLocation ? getDistance(userLocation, coordinates) : 0;
-
   return distance >= 1000
     ? `${(distance / 1000).toFixed(1)} Km`
     : `${distance} m`;

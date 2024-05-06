3c5754a5abcedc0919e67622ec6222a7b9b12577
OlehSirbu <oleh@bookinglayer.com>
2024-05-06 11:48:19 +0200

    Update TotalButton styling to fix positioning issue

--- src/components/TotalButton.tsx
+++ src/components/TotalButton.tsx
@@ -10,7 +10,7 @@ const TotalButton = ({
   return (
     <button
       onClick={onClick}
-      className="w-3/4 md:w-1/3 bg-[#262638] rounded-[0.625rem] p-3 text-white text-lg uppercase border-none fixed left-1/2 transform -translate-x-1/2 -translate-y-1/2 bottom-8 flex w-3/4 md:w-1/3 justify-center"
+      className="w-3/4 md:w-1/3 bg-[#262638] rounded-[0.625rem] p-3 text-white text-lg uppercase border-none fixed left-1/2 transform -translate-x-1/2 -translate-y-1/2 bottom-2 flex w-3/4 md:w-1/3 justify-center"
     >
       TOTAL ({formatAsEuro(total)})
     </button>
